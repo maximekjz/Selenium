9ebf5a32016087a62ec137541fba4d3a515d07c6
maximekjz <maximekajzer@gmail.com>
2024-09-26 03:06:59 -0700

    Initial commit

---
+++ Config/config.py
@@ -0,0 +1,11 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = '/Users/maximekajzer/Desktop/Google\ Chrome'
+
+    BASE_URL = 'https://auth.eurosport.com/login?flow=login'
+    USER_NAME = 'testseleniummaxime@gmail.com'
+    PASSWORD = 'Testselenium8'
+    LOGIN_PAGE_TITLE = 'Eurosport'
+    HOME_PAGE_TITLE = 'Sport news, live streaming &amp; results - Eurosport'
+    HOME_PAGE_HEADER = 'Eurosport'
+    ACCOUNT_NAME = 'maximekajzer'
+
---
+++ Pages/HomePage.py
@@ -0,0 +1,35 @@
+from selenium.webdriver.common.by import By
+from Pages.BasePage import BasePage
+
+
+class HomePage(BasePage):
+
+    """Check with class"""
+    HEADER = (By.CSS_SELECTOR, 'a.flex items-center self-center')
+    MENU = (By.CSS_SELECTOR, 'BurgerMenuButton')
+    SEARCH = (By.CSS_SELECTOR, 'a.flex relative items-center bg-transparent px-3 transition-colors hover:bg-fill-03 active:bg-fill-02')
+
+    """Other way to check"""
+    WATCH_BUTTON = (By.CSS_SELECTOR, "a[href='/watch/'][data-testid='link-organism-mobile-sub-navigation-item']")
+
+    def __init__(self, driver):
+        super().__init__(driver)
+
+    def get_home_page(self, title):
+        return self.get_title(title)
+
+    def is_watch_button_exists(self):
+        return self.is_visible(self.WATCH_BUTTON)
+
+    def is_menu_exists(self):
+        return self.is_visible(self.MENU)
+
+    def is_search_exists(self):
+        return self.is_visible(self.SEARCH)
+
+    def get_header_value(self):
+        if self.get_element_text(self.HEADER):
+            return self.HEADER
+
+
+
